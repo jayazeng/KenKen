258190c92f24e66e3b248903ea0eacf8461ebeb5
Cedric Williams <me@cedricwilliams.com>
2019-09-15 10:57:43 -0400

    Still working

---
+++ src/kenken/StartKenKen.java
@@ -0,0 +1,18 @@
+package kenken;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class StartKenKen {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner inFile = new Scanner(new FileReader("\\src\kenken.txt"));
+
+		
+		
+		
+	}
+
+}
