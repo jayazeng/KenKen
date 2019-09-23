0161e58414b15dbf685c6a7b154047d0e4d07fe2
Cedric Williams <me@cedricwilliams.com>
2019-09-22 20:26:40 -0400

    Add comments to Advanced BackTrack

--- src/kenken/AdvancedBackTrack.java
+++ src/kenken/AdvancedBackTrack.java
@@ -7,4 +7,17 @@ public class AdvancedBackTrack extends SimpleBackTrack {
 		
 	}
 
+	// runs right after datafile is imported to reduce possibilities
+	public void forwardChecking() {
+		
+		
+		
+	}
+	
+	// takes in current domain and reduces possibilities by what the total can be
+	public void limitCageDomain() {
+		
+		
+	}
+	
 }
