d7c8c18ea3cc8b8dbe599884d1fcb9777c6facf4
Cedric Williams <me@cedricwilliams.com>
2019-09-24 21:00:12 -0400

    Got PreCheck working, may reduce later but it works

--- src/kenken/AdvancedBackTrack.java
+++ src/kenken/AdvancedBackTrack.java
@@ -1,147 +1,156 @@
+/* This class controls the Advanced Back Track algorithm which is a subclass or extension of SimpleBackTrack.
+ * First we establish a hashtable to store a cage letter <key> and a set of potential numbers which fit the total 
+ * and operator constraint.  This is done before the algorithm solves the puzzle.
+ * 
+ * We use an ARC Consistency to eliminate the domains prior to running the solve algorithm.
+ * 
+ * Our forward checking algorithm
+ */
+
 package kenken;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class AdvancedBackTrack extends SimpleBackTrack {
-
-	//int n; // length and width of array
 	
 	private InputFile input; // inputfile that will have all the data
 
-	Hashtable<String,ArrayList<Object>> cageTD ; //Cage Total & Domain
+	String op;
+	
+	String letter;
+	
+	int n;
+	
+	int cageTotal;
+	
+	ArrayList<Object> tmp; 
+	
+	Hashtable<String,ArrayList<Object>> cageDomain = new Hashtable<String,ArrayList<Object>>(); //Collection - letter as key and Cage Total & Domain
 	
 	public AdvancedBackTrack(InputFile file) {
 		super(file);
 		
 		input = file;
 		
-		//setTotalOps(input.);
+		n = input.n;
+		
+		preCheck(input);
 		
 		
 		System.out.print("hello");
 		
 	}
 
+
 	
-	
-	// takes in current domain and reduces possibilities by what the total can be
-	public void limitCageDomain() {
-		
-	
-		
-		
-	}
-	
-	private void setTotalOps( String line) {
-		
-		int lineLength = line.length(); // need to check the line length to know where to find the operator
-		
-		String op = parseOp(lineLength,line,"op"); // line.substring(lineLength-1) finds the operator
-		
-		int total = Integer.parseInt(parseOp(lineLength,line,"total")); // finds the total value
-		
-		String letter = line.substring(0,1);
-		
-		input.cages.get(letter).op = op; // inserts eachCage into hashtable(cages) if its not there
-		
-		input.cages.get(letter).total = total;
-		
-		cageTD.put(letter, forwardChecking(letter,total,op,n));
-		
-	}
-	
-	private String parseOp(int lineLength, String line, String type) {
-		
-		
-		if(type=="op" && !Character.isDigit(line.charAt(lineLength-1))) {
-	
-			
-			return line.substring(lineLength-1);
-		
-		} else
-		
-			if(type=="op" && Character.isDigit(line.charAt(lineLength-1))) {
-	
-			
-			return "=";
-		}
-		
-		
-		if(type=="total" && !Character.isDigit(line.charAt(lineLength-1))) {
-			
-			return line.substring(2,lineLength-1);
-		} 
-		
-		return line.substring(2);
-	}
+
 	 
-	
-	public ArrayList<Object> forwardChecking(String letter, int x, String op,int n) {
+	// Inputs a cell location , total, operator and dimension size (n) and determines 
+	// possible values for each cage.
+	public void preCheck(InputFile input) {
 		
-		ArrayList<Object> tmp = new ArrayList<Object>();
+		String key;
+		String letter;
 		
-		// Add
-		if(op=="+") {
 		
-			// loop thru and only get numbers < total
-			
-			for(int q = 1; q < x;q++) {
-				
-				
-				
-			}
-			
-			return tmp;
-		}
-		// Subtract
-		if(op=="-") {
-			
-			for(int q = 1;q<n;q++) {
-				
-				
-			// loop thru and grab number pairs that equal total
-				// numbers could be higher and lower than total
-				
-			}
-			
-			return tmp;
-		}
-		// Multiply
-		if(op=="*") {
-			
-			for(int q = 1;q<=x;q++) {
-				
-				
-			// loop thru and grab number pairs that have a mod of zero of total
-				
-				
-			}
-			
-			return tmp;
-		}
-		// Division
-		if(op=="/") {
-			
-			for(int q = 1;q<=n;q++) {
-				
-				
-			// loop thru and grab number pairs that have a mod of zero of total
-				
-				
-			}
-			
-			return tmp;
-			
-		}
-		// Equals
-		if(op=="=") {
-			
-			tmp.add(x);
-			
-		}
-		return tmp;
-	}
+		
+		
+		for(int i = 1; i <= n;i++) {
+			for(int j = 1;j<=n;j++) {
 	
+				tmp = new ArrayList<Object>(15);
+				
+				key = "("+i+","+j+")";
+				
+				letter = input.cageLookup.get(key);
+				
+				cageTotal = input.cages.get(letter).getTotal();
+				
+				op = input.cages.get(letter).getOp();
+				
+					// Add
+					if(op.equals("+")) {
+					
+						// loop thru and only get numbers < total
+						
+						for(int q = 1; q <= n;q++) {
+							
+							tmp.add(q);
+							
+						}
+						
+						
+						
+						
+					}
+					// Subtract
+					if(op.equals("-")) {
+						
+						for(int q = 1;q<=n;q++) {
+							
+							
+						// loop thru and grab number pairs that equal total
+							// numbers could be higher and lower than total
+							
+							tmp.add(q);
+							
+						}
+						
+						
+					}
+					// Multiply
+					if(op.equals("*")) {
+						
+						for(int q = 1;q<=n;q++) {
+							
+							
+						// loop thru and grab number pairs that have a mod of zero of total
+							
+							
+							if(cageTotal % q == 0) {
+								
+								tmp.add(q);
+								
+							}
+							
+							
+						}
+						
+						
+					}
+					// Division
+					if(op.equals("/")) {
+						
+						for(int q = 1;q<=n;q++) {
+							
+							
+						// loop thru and grab number pairs that have a mod of zero of total
+							if(cageTotal * q <= n || cageTotal % q ==0 || q % cageTotal == 0) {
+								
+								tmp.add(q);
+								
+							}
+							
+						}
+						
+						
+						
+					}
+					// Equals
+					if(op.equals("=")) {
+						
+						tmp.add(cageTotal);
+						
+					}
+					
+					cageDomain.put(key, tmp);
+					
+	}}  //end of i & j loop
+			
+		
+		System.out.print("Cedric");
+		
+	} //end of preCheck
 	
-}
+}  // end of class
